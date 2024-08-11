481355ead945397da170b6d0d214cce33ffc23cb
Hiralal Singh <hiralals221@gmail.com>
2024-08-11 11:07:10 +0530

    uploading the main file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DomNom',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='A simple Python package for routing and launching web applications',
+    author='Hiralal Singh',
+    author_email='successor.smma@gmail.com',
+    url='https://github.com/your-repo/DomNom',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
